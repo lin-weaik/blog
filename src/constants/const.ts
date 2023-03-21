1d0750bdd6f516a6e1dd10be1f62b52c5d0901e8
DLIN <321603776@qq.com>
2023-03-21 16:29:09 +0800

    add NEXT_PUBLIC_CONVEX_URL const to const.ts

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -1,1 +1,2 @@
 export const NEXT_PUBLIC_RESUME_ID = process.env.NEXT_PUBLIC_RESUME_ID as string;
+export const NEXT_PUBLIC_CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL as string;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ChakraProvider } from '@chakra-ui/react'
+import { NEXT_PUBLIC_CONVEX_URL } from '@/constants/const';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+const convex = new ConvexReactClient(NEXT_PUBLIC_CONVEX_URL);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
